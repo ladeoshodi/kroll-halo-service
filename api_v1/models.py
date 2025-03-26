f4de6700fa5ce1ea7d4477a20c1c2b572093a440
Lade Oshodi <13224175+ladeoshodi@users.noreply.github.com>
2025-03-26 15:58:30 +0000

    Create Initial Model

--- api_v1/models.py
+++ api_v1/models.py
@@ -3,12 +3,10 @@ from django.db import models
 
 # Create your models here.
 class KrollHaloMapping(models.Model):
-    kroll_ticket_id = models.StringField(max_length=255)
-    halo_ticket_id = models.StringField(max_length=255)
+    kroll_ticket_id = models.CharField(max_length=255, unique=True)
+    halo_ticket_id = models.CharField(max_length=255, unique=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
     class meta:
-        db_table = "kroll_halo_mapping"
-        unique_together = ("kroll_id", "halo_id")
         ordering = ["created_at"]
--- kroll_halo_service/settings.py
+++ kroll_halo_service/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    "api_v1.apps.ApiV1Config",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
